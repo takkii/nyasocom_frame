8a7c3e0659c2bb21c2fe4ff0be2316690be95bc0
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-12-10 15:46:46 +0900

    Update.

---
+++ lib/install.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+#!/usr/bin/ruby
+
+require 'open3'
+require 'fileutils'
+
+# Installer runner.
+class InstallerRunner
+  # default encoding utf-8, change encode here.
+  def self.encoding_style
+    Encoding.default_internal = 'UTF-8'
+    Encoding.default_external = 'UTF-8'
+  end
+
+  def self.run
+    encoding_style
+    one = ARGV[0]
+    FileUtils.mkdir_p(File.expand_path("~/#{one}"))
+    FileUtils.cd(File.expand_path("~/#{one}"))
+    system("git clone git@github.com:takkii/nyasocom_oss.git .")
+    FileUtils.rm_rf(File.expand_path("~/#{one}/.git"))
+  end
+end
+
+InstallerRunner.run
