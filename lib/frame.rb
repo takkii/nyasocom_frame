a8092167fd5710abcdcbdcc5a278d6b930b0b6e9
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-12-11 09:07:39 +0900

    Update.

---
+++ lib/frame.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+lib = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+# Heat module
+module Heart
+  module_function
+
+  def version
+    require 'frame/version'
+    print Frame::VERSION
+    puts ' : [nyasocom_framework_version] '
+  end
+
+  def installer
+    require 'install'
+  end
+
+  def default
+    puts "nyasocom_frame is Copyright © 2022 Takayuki, Kamiyama."
+  end
+
+  def document
+    puts text = <<-EOS
+# ライセンス情報
+heat
+
+# バージョン表記
+heat -v
+
+# ひな形自動生成
+heat new [フォルダ名]
+heat new example
+※ ~/ 先に指定のフォルダ名で展開されます。
+
+# ヘルプ、ドキュメント
+heat -h
+
+EOS
+
+  end
+end
+
+v = /\A[-][v]\z/
+h = /\A[-][h]\z/
+n = /\Anew\z/
+
+one = ARGV[0]
+
+include Heart
+
+if one.nil?
+  default
+elsif one.match?(h)
+  document
+elsif one.match?(n)
+  installer
+elsif one.match?(v)
+  version
+else
+  puts '該当のオプションがありません、ドキュメントを参照してください。'
+end
+
+__END__
