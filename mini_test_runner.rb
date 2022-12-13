a70f38e82989232ade432ead6b2a82f7e163f38f
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-12-13 12:47:05 +0900

    Update.

--- mini_test_runner.rb
+++ mini_test_runner.rb
@@ -3,15 +3,31 @@
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-# Mini Test Auto Runner
+# Mini Test Runner
+# class Auto_Runner
+#   def self.ruby
+#     require 'auto_runner'
+#   end
+# end
+
 class Mini_Runner
   def self.ruby
     require 'mini_auto_runner'
   end
 end
 
+# begin
+#   Auto_Runner.ruby
+#   Auto_Runner(true, "#{Dir.home}/GitHub/nyasocom_frame/mini_test")
+# rescue StandardError => e
+#   puts e.backtrace
+# ensure
+#   GC.compact
+# end
+
 begin
-  require 'minitest-runners'
+  Mini_Runner.ruby
+  Mini_Runner(true, "#{Dir.home}/GitHub/nyasocom_frame/mini_test")
 rescue StandardError => e
   puts e.backtrace
 ensure
