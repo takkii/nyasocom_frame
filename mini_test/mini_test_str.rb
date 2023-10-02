f35be92a2879e90f5ecd82b2ae6054fd32331bc0
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-10-02 17:47:18 +0900

    Delete.

--- mini_test/mini_test_str.rb
+++ mini_test/mini_test_str.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require 'minitest/reporters'
 
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
