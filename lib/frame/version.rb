ee0888ce4459ae713e1806e85b03408a7287687e
TAKAYUKI KAMIYAMA <takkii@TAKAYUKInoiMac.local>
2024-09-12 11:45:58 +0900

    Update.

--- lib/frame/version.rb
+++ lib/frame/version.rb
@@ -5,7 +5,7 @@ require 'date'
 begin
 # nyasocom_frame version = x.x.x-xxxx.xx.xx
 module Frame
-  frame_version = '1.0.9'
+  frame_version = '1.0.10'
   t = Date.today
   build_day = t.strftime('%Y.%m.%d')
   VERSION = "#{frame_version}-#{build_day}".freeze
