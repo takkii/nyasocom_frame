266f016dfc182620f8a15246e943fe6a73a11c90
TAKAYUKI KAMIYAMA <takkii@TAKAYUKInoiMac.local>
2024-09-12 11:46:16 +0900

    Update.

--- lib/download.rb
+++ lib/download.rb
@@ -5,14 +5,13 @@ require 'fileutils'
 
 # Installer runner.
 class InstallerRunner
-  # default encoding utf-8, change encode here.
-  def self.encoding_style
-    Encoding.default_internal = 'UTF-8'
-    Encoding.default_external = 'UTF-8'
-  end
-
   def self.run
     encoding_style
+    nyasocom_command
+  end
+  
+  private
+  def self.nyasocom_command
     if system('git clone git@github.com:takkii/nyasocom_oss.git', exception: true)
     else
       system('git clone https://github.com:takkii/nyasocom_oss.git')
@@ -20,6 +19,16 @@ class InstallerRunner
     FileUtils.cd("./nyasocom_oss")
     FileUtils.rm_rf("./.git")
     FileUtils.rm_rf("./.github")
+    puts <<-EOF
+
+Cloned nyasocom_oss with nyasocom_frame.
+
+EOF
+  end
+
+  def self.encoding_style
+    Encoding.default_internal = 'UTF-8'
+    Encoding.default_external = 'UTF-8'
   end
 end
 
